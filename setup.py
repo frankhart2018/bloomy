f2c442db17cd4f55de29adc4637eae96fbc63d90
frankhart2018 <sdhargithub@gmail.com>
2024-06-14 23:09:34 -0500

    Add create, drop, flush, info, and list operations

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+import codecs
+import pathlib
+from setuptools import setup, find_packages
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name="bloomy",
+    version=get_version("bloomy/__init__.py"),
+    packages=find_packages(),
+    install_required=["pydantic"],
+)
